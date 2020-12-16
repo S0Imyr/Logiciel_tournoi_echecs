631b42eba874495049d8e63e1ef552c4ac6ab81d
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-12-16 20:45:24 +0100

    In actor.py and game.py : class improvement

--- models/actor.py
+++ models/actor.py
@@ -1,22 +1,24 @@
+ID_WIDTH = 8
 
 
 class Player:
-    def __init__(self):
-        self.id_player
-        self.last_name = ""
-        self.first_name = ""
-        self.birth_date = ""
-        self.gender = ""
-        self.rank
-        self.opponents=[]
-        self.id_person
+    last_id_person = "0"*8
+    def __init__(self, player_id, last_name, first_name, birthdate, gender, rank):
+        self.player_id = player_id
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birthdate = birthdate
+        self.gender = gender
+        self.rank = rank
+        self.opponents = []
+        self.person_id = (ID_WIDTH + 1-len(str(Player.last_person_id)))*"0"+str(int(Player.last_person_id.lstrip('0'))+1)
 
 
 class Person:
-    def __init__(self):
-        self.id_person
-        self.last_name = ""
-        self.first_name = ""
-        self.birth_date = ""
-        self.gender = ""
-        self.role = ""
+    def __init__(self, person_id, last_name, first_name, birthdate, gender, role):
+        self.person_id = person_id
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birthdate = birthdate
+        self.gender = gender
+        self.role = role
--- models/game.py
+++ models/game.py
@@ -1,22 +1,22 @@
-
+ID_WIDTH = 8
 
 class Tournament:
-    def __init__(self):
-        self.id_tournament
-        self.name = ""
-        self.location = ""
-        self.start_date = ""
-        self.end_date = ""
+    last_tournament_id = "0"*ID_WIDTH
+    def __init__(self, name, location, start_date, end_date):
+        self.tournament_id = (ID_WIDTH+1-len(str(Tournament.last_tournament_id)))*"0"+str(int(Tournament.last_tournament_id.lstrip('0'))+1)
+        self.name = name
+        self.location = location
+        self.start_date = start_date
+        self.end_date = end_date
         self.number_of_rounds = 4
-        self.rounds = ""        # La liste des instances de tours.
-        self.players = ""       # Liste des indices correspondant aux instances du joueur stockées en mémoire)
-        self.timer = ""         # C'est toujours un bullet, un blitz ou un coup rapide)
-        self.description = ""   # Les remarques générales du directeur du tournoi vont ici).
+        self.rounds = "" # La liste des instances de tours.
+        self.list_of_players = "" # Liste des indices correspondant aux instances du joueur stockées en mémoire)
+        self.timer_type = "" # C'est toujours un bullet, un blitz ou un coup rapide)
+        self.description = "" # Les remarques générales du directeur du tournoi vont ici).
 
 class Round:
-    def __init__(self):
-        self.id_round
-        self.tournament_id
+    def __init__(self, tournament_id):
+        self.tournament_id = tournament_id
         self.number = 0
         self.players = []
         self.players_rank = []
