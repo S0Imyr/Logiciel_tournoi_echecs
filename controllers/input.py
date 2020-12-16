b4a1d6f4bc26efa9e744162d67018fa9fd4a38cf
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-12-16 22:37:15 +0100

    controllers/ input.py : construction of function to prompt a date and an other to prompt a number

--- controllers/input.py
+++ controllers/input.py
@@ -1,3 +1,9 @@
+import datetime
+
+
+DATE_FORMAT = ["day", "month", "year"]
+
+
 def prompt_number(message, min=None, max=None):
     """Demande à l'utilisateur à l'aide de
     message de saisir un entier.
@@ -7,7 +13,6 @@ def prompt_number(message, min=None, max=None):
     entre le min et le max (non compris),
     alors la question est reposée.
     """
-
     response = input(message)
     response_is_not_number = True
     while response_is_not_number:
@@ -49,3 +54,35 @@ def prompt_date(message):
     """Demande à l'utilisateur à l'aide de
     message de saisir une date
     """
+    day = 1
+    month = 1
+    year = 0
+    response_is_not_date = True
+    while response_is_not_date:
+        response = input(message)
+        response_is_not_slashed = True
+        response_is_not_int = True
+        response_is_not_yyyy = True
+        date = response.split("/")
+        if len(date) == len(DATE_FORMAT) :
+            response_is_not_slashed = False
+            try :
+                day = int(date[0])
+                month = int(date[1])
+                if len(date[2]) == 4:
+                    response_is_not_yyyy = False
+                    year = int(date[2])
+                else:
+                    message = "Erreur de format, veuiller écrire la date en respectant le format: jj/mm/aaaa: "
+                response_is_not_int = False
+            except ValueError:
+                response_is_not_int = True
+                message = "Erreur de format, veuiller écrire la date en respectant le format: jj/mm/aaaa: "
+        else:
+            message = "Erreur de format, veuiller écrire la date en respectant le format: jj/mm/aaaa: "
+        response_is_not_date = response_is_not_int or response_is_not_slashed or response_is_not_yyyy
+    return datetime.date(year,month,day)
+
+if __name__ == "__main__":
+    date = prompt_date("Entrer votre date de naissance: ")
+    print(date)
