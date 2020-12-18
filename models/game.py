637a9c19b215335553bedf75228b86d639fbb24d
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-12-18 12:00:02 +0100

    Models/game.py : separation Player (in a Tournament) and Actor In Round, construction of ranking_players method

--- models/game.py
+++ models/game.py
@@ -1,18 +1,24 @@
+from operator import attrgetter
+
+
+FIRST = 1
 ID_WIDTH = 8
+NB_PLAYERS = 8
+NB_ROUND = 4
 POINTS = {"victory": 1, "draw": 0.5, "defeat": 0}
 
 
-class Player:
+class Actor:
     last_player_id = "0"*ID_WIDTH
 
     def __init__(self, last_name, first_name, birthdate, gender, rank):
-        if Player.last_player_id.lstrip('0') == "":
-            Player.last_player_id = str(1)
+        if Actor.last_player_id.lstrip('0') == "":
+            Actor.last_player_id = str(1)
         else:
-            Player.last_player_id = str(int(Player.last_player_id.lstrip('0'))+1)
-        self.player_id =\
-            (ID_WIDTH + 1 - len(Player.last_player_id.lstrip('0')))*"0"\
-            + Player.last_player_id
+            Actor.last_player_id = str(int(Actor.last_player_id.lstrip('0')) + 1)
+        self.player_id = \
+            (ID_WIDTH + 1 - len(Actor.last_player_id.lstrip('0'))) * "0" \
+            + Actor.last_player_id
         self.last_name = last_name
         self.first_name = first_name
         self.birthdate = birthdate
@@ -21,12 +27,20 @@ class Player:
         self.opponents = []
 
 
+class Player:
+    def __init__(self, rank):
+        self.rank = rank
+        self.points = 0
+        self.ranking = 0
+
+
 class Match:
     def __init__(self, match_nb, round_nb, tournament_id, player1, player2):
         self.match_nb = match_nb
         self.round_nb = round_nb
         self.tournament_id = tournament_id
-        self.players = [player1, player2]
+        self.player1 = player1
+        self.player2 = player2
         self.players_points = [0, 0]
         self.winner = None
         self.finished = False
@@ -51,18 +65,20 @@ class Match:
             elif self.winner == 2:
                 self.players_points = [POINTS["defeat"], POINTS["victory"]]
             self.points_assigned = True
+            self.player1.points += self.players_points[0]
+            self.player2.points += self.players_points[1]
 
 
 class Round:
-    def __init__(self, round_nb, tournament_id, players, players_points, players_rank):
+    def __init__(self, round_nb, tournament_id, players):
         self.tournament_id = tournament_id
         self.round_nb = round_nb
         self.players = players
-        self.players_points = players_points
-        self.players_rank = players_rank
 
-    def rank_players(self):
-        pass
+    def ranking_players(self):
+        sorted_players = sorted(self.players, key=attrgetter("points", "rank"))
+        for rank in range(NB_PLAYERS):
+            sorted_players[rank].ranking = rank
 
     def define_matches(self):
         pass
@@ -79,7 +95,7 @@ class Tournament:
         self.location = location
         self.start_date = start_date
         self.end_date = end_date
-        self.number_of_rounds = 4
+        self.number_of_rounds = NB_ROUND
         self.rounds = "" # La liste des instances de tours.
         self.list_of_players = "" # Liste des indices correspondant aux instances du joueur stockées en mémoire)
         self.timer_type = "" # C'est toujours un bullet, un blitz ou un coup rapide)
@@ -111,3 +127,8 @@ if __name__ == "__main__":
     print(match1.players_points)
     """
     """Tests Round"""
+    print(sorted({"Anakin": 1, "Ahsoka": 45, "Obiwan": 58, "Plokoon": 15}.items(), key=lambda t: t[1]))
+    k = list({"Anakin": 1, "Ahsoka": 45, "Obiwan": 58, "Plokoon": 15}.values())
+    print(k)
+    k.sort()
+    print(k)
