5834770d9104aa1f06e6a6737959412ad94267d1
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-12-31 12:28:19 +0100

    match.py : correction of a mistake in previous commit

--- chess/models/match/match.py
+++ chess/models/match/match.py
@@ -2,6 +2,9 @@ POINTS = {"victory": 1, "draw": 0.5, "defeat": 0}
 
 
 class Match:
+    """
+    A Match is one of the 4 duel in a round
+    """
     def __init__(self, match_nb, round_nb, tournament_id):
         self.match_nb = match_nb
         self.round_nb = round_nb
@@ -13,24 +16,49 @@ class Match:
         self.finished = False
         self.points_assigned = False
 
+    def __repr__(self):
+        repr = f"Match {self.match_nb + 1}: \n" \
+              f"1: {self.player1.name} vs 2:{self.player2.name}\n"
+        winner = "Aucun"
+        if self.finished:
+            if self.winner == 1:
+                winner = self.player1.name
+            if self.winner == 2:
+                winner = self.player2.name
+            repr += f"Le match a été remporté par {winner} \n"
+        return repr
+
     def declare_result(self, num_player):
+        """
+        Declare the winner by assigning the winner's number
+         to the winner attribute
+        :param num_player: 1 for the first quoted, 2 for the second.
+         0 when it's a draw game
+        :return: None
+        """
         if self.finished:
             print("Already registered")
         else:
-            if num_player is not None:
-                self.winner = num_player
-                self.finished = True
+            self.winner = num_player
+            self.finished = True
 
     def assign_points(self):
-        if self.points_assigned:
-            print("Already assigned")
+        """
+        Assign points to the players
+        The function test if points are already assign, and then
+        assign the points
+        :return:
+        """
+        if self.winner == 0:
+            self.player1.points = POINTS["draw"]
+            self.player2.points = POINTS["draw"]
         else:
             if self.winner is None:
                 self.players_points = [POINTS["draw"], POINTS["draw"]]
             elif self.winner == 1:
-                self.players_points = [POINTS["victory"], POINTS["defeat"]]
+                self.player1.points = POINTS["victory"]
+                self.player2.points = POINTS["defeat"]
             elif self.winner == 2:
-                self.players_points = [POINTS["defeat"], POINTS["victory"]]
+                self.player1.points = POINTS["defeat"]
+                self.player2.points = POINTS["victory"]
             self.points_assigned = True
-            self.player1.points += self.players_points[0]
-            self.player2.points += self.players_points[1]
