8e80986333aebf353e3fce641f51bfa7eede87a5
Raphael LOSSEC <solmyr-ars@laposte.net>
2020-12-31 12:58:52 +0100

    creation of constant.py for constant values

--- chess/models/game.py
+++ chess/models/game.py
@@ -1,13 +1,8 @@
 import datetime
-
-FIRST = 1
-ID_WIDTH = 8
-NB_PLAYERS = 8
-NB_MATCH = 4
-NB_ROUND = 4
-POINTS = {"victory": 1, "draw": 0.5, "defeat": 0}
-MATCH_1ST_ROUND = [[1, 5], [2, 6], [3, 7], [4, 8]]
-MATCH_OTHER_ROUND = [[1, 2], [3, 4], [5, 6], [7, 8]]
+import constant
+import actors
+import match
+import round
 
 
 class Tournament:
