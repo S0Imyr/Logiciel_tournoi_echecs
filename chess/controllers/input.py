ff7fa3371c1158cf524ee52756ac2c8600d1a5da
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-04 15:47:53 +0100

    input.py : correction for prompt_number and creation of input_actor for profil creation

--- chess/controllers/input.py
+++ chess/controllers/input.py
@@ -1,17 +1,22 @@
 import datetime
-
+from chess.models.actors import Actor
 
 DATE_FORMAT = ["day", "month", "year"]
 
 
 def prompt_number(message, min=None, max=None):
-    """Demande à l'utilisateur à l'aide de
+    """
+    Demande à l'utilisateur à l'aide de
     message de saisir un entier.
 
     Si l'utilisateur fournit un objet qui n'est
     pas un entier, ou un entier qui n'est pas
     entre le min et le max (non compris),
     alors la question est reposée.
+    :param message:
+    :param min:
+    :param max:
+    :return:
     """
     response = input(message)
     response_is_not_number = True
@@ -22,19 +27,59 @@ def prompt_number(message, min=None, max=None):
         except ValueError:
             response = input("Entrée incorrecte, veuillez entrer un nombre : ")
 
-    while not (int(response) >= min and int(response) <= max):
-        response = input(f"Nombre incorrect, veuillez entrer un entier entre {min} et {max} : ")
+    if min is None and max is None:
+        pass
+    elif min is None:
+        while not int(response) < max:
+            response = input(f"Nombre incorrect, veuillez"
+                             f" entrer inférieur à {max} : ")
+    elif max is None:
+        while not int(response) > min:
+            response = input(f"Nombre incorrect, veuillez"
+                             f" entrer supérieur à {min} : ")
+    else:
+        while not (int(response) > min and int(response) < max):
+            response = input(f"Nombre incorrect, veuillez"
+                             f" entrer un entier entre {min} et {max} : ")
     return int(response)
 
 
 def prompt_string(message):
-    """Demande à l'utilisateur à l'aide de
-    message de saisir une chaine de caractère.
     """
+    Demande à l'utilisateur de saisir une chaine de caractère.
+    Renvoie la chaîne avec majuscule
+    :param message:
+    :return: input
+    """
+    response = input(message)
+    response = response.capitalize()
+    return response
+
+
+def prompt_gender(message):
+    """
+    Demande à l'utilisateur de spécifier un genre.
+    Si la réponse n'est pas M ou F, la demande
+    est refaite.
+    :param message:
+    :return: input
+    """
+    response = input(message)
+    while response != "M" and response != "F":
+        input("Votre réponse ne correspond pas. \n"
+              "Veuillez indiquer 'F' pour Féminin "
+              "ou 'M' pour Masculin :")
+    return response
+
 
 def prompt_date(message):
-    """Demande à l'utilisateur à l'aide de
-    message de saisir une date
+    """
+    Demande à l'utilisateur à l'aide de
+    message de saisir une date selon un format.
+    Si ce format n'est pas respecté, la demande
+    est refaite.
+    :param message:
+    :return: input date
     """
     day = 1
     month = 1
@@ -46,9 +91,9 @@ def prompt_date(message):
         response_is_not_int = True
         response_is_not_yyyy = True
         date = response.split("/")
-        if len(date) == len(DATE_FORMAT) :
+        if len(date) == len(DATE_FORMAT):
             response_is_not_slashed = False
-            try :
+            try:
                 day = int(date[0])
                 month = int(date[1])
                 if len(date[2]) == 4:
@@ -63,8 +108,27 @@ def prompt_date(message):
         else:
             message = "Erreur de format, veuiller écrire la date en respectant le format: jj/mm/aaaa: "
         response_is_not_date = response_is_not_int or response_is_not_slashed or response_is_not_yyyy
-    return datetime.date(year,month,day)
+    return datetime.date(year, month, day)
+
+
+def input_actor():
+    """
+    Demande les informations d'un joueur afin de
+     les regrouper dans son instance acteur
+    :return: instance d'acteur
+    """
+    print("Inscription d'un joueur: \n")
+    print("Renseigner les informations suivantes: \n")
+    last_name = prompt_string("Votre nom de famille : ")
+    first_name = prompt_string("Votre prénom : ")
+    birthdate = prompt_date("Votre date de naissance en respectant le format: jj/mm/aaaa: ")
+    gender = prompt_gender("Votre genre en écrivant F pour féminin ou M pour Masculin: ")
+    rank = prompt_number("Votre classement HATP: ", min=0)
+    acteur = Actor(last_name, first_name, birthdate, gender, rank)
+    return acteur
 
 if __name__ == "__main__":
-    date = prompt_date("Entrer votre date de naissance: ")
-    print(date)
+    Me = input_actor()
+    print(Me)
+    You = input_actor()
+    print(You)
