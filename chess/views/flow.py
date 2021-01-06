bc06dfa8833b26bbb6a15373a6223823ac24744d
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-06 17:16:32 +0100

    flow.py : decoration

--- chess/views/flow.py
+++ chess/views/flow.py
@@ -1,13 +1,19 @@
 
 
-def intro_tournament():
-    print("\n ### Tournoi ### \n"
-          "\n"
-          "-- Débutons par définir les joueurs : --\n")
-
 def intro_HomeMenu():
     print("\n ### Menu Principal ### \n"
           "\n"
           "-- Que souhaitez vous réaliser ? --\n")
 
+
+def tournament_creation():
+    print("\n ### Création du tournoi ### \n"
+          "\n"
+          "-- Débutons par les informations du tournoi : --\n")
+
+
+def tournament_players():
+    print("\n ### Joueurs du tournoi ### \n"
+          "\n"
+          "-- Maintenant définissons les joueurs : --\n")
 
