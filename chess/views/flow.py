9dc9ac837923f0ed1045e4b471dfcbfa2a53214b
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-07 14:30:01 +0100

    flow.py : view_round_matchs

--- chess/views/flow.py
+++ chess/views/flow.py
@@ -1,3 +1,4 @@
+NB_MATCH = 4
 
 
 def view_intro_home_menu():
@@ -13,19 +14,19 @@ def view_tournament_creation():
 
 
 def view_tournament_players(tournament):
-    print(f"\n ### Tournoi {tournament}: Type d'entrée des joueurs ### \n"
+    print(f"\n ### Tournoi {tournament.name}: Type d'entrée des joueurs ### \n"
           "\n"
           "-- Maintenant comment voulez vous entrer un joueur ? --\n")
 
 
 def view_new_player(tournament=None):
-    message = ""
+    display = ""
     if tournament:
-        message += f"\n ### Tournoi {tournament}: Nouveau joueur ### \n "
+        display += f"\n ### Tournoi {tournament.name}: Nouveau joueur ### \n "
     else:
-        message += "Nouveau joueur"
-    message += "\n -- Entrez les informations demandées: --\n"
-    print(message)
+        display += "Nouveau joueur"
+    display += "\n -- Entrez les informations demandées: --\n"
+    print(display)
 
 
 def view_validation_new_player(actor):
@@ -35,6 +36,35 @@ def view_validation_new_player(actor):
 
 
 def view_id_player(tournament, num=1,):
-    print(f"\n ### Tournoi {tournament}: Identification de joueur ### \n"
+    print(f"\n ### Tournoi {tournament.name}: Identification de joueur ### \n"
           "\n")
     print(f"-- Joueur {num}--")
+
+
+def view_launch_tournament(tournament):
+    print(f"\n ### Lancement du Tournoi {tournament.name}: ### \n"
+          "\n")
+
+
+def view_round_matchs(round):
+    display = f"\n ### Matchs du Tour {round.round_nb} " \
+              f"du Tournoi {round.tournament_id} : ### \n"
+    if round.matchs != {}:
+        if round.finished:
+            display += f"Les matchs ont vu s'affronter : \n"
+        else:
+            display += f"Les matchs verront s'affronter : \n"
+        for num_match in range(NB_MATCH):
+            display += f"{num_match + 1}. " \
+                       f"{round.matchs[num_match].player1.name}" \
+                       f" et {round.matchs[num_match].player2.name} \n"
+            if round.finished:
+                if round.matchs[num_match].winner == 0:
+                    display += f"Match nul. \n"
+                if round.matchs[num_match].winner == 1:
+                    display += f"Victoire de " \
+                               f"{round.matchs[num_match].player1.name}. \n"
+                if round.matchs[num_match].winner == 2:
+                    display += f"Victoire de " \
+                               f"{round.matchs[num_match].player2.name}. \n"
+    print(display)
