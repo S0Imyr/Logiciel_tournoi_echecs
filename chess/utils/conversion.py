9227c359d69490059886a47001bc07ee139d8319
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-08 18:52:34 +0100

    conversion.py : conversions between lists and string and some tests

--- chess/utils/conversion.py
+++ chess/utils/conversion.py
@@ -7,3 +7,21 @@ def str_into_date(str_date):
     month = int(date[1])
     year = int(date[2])
     return datetime.date(year, month, day)
+
+def list_to_str_space(list):
+    string = ""
+    for element in list:
+        string += str(element)+ " "
+    return string.strip()
+
+def str_space_to_int_list(string):
+    string_list = string.split(" ")
+    int_list = []
+    for element in string_list:
+        int_list.append(int(element))
+    return int_list
+
+if __name__ == "__main__":
+    test = list_to_str_space([2, 1])
+    print(test)
+    print(str_space_to_int_list(test))
