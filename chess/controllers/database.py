25af443db8cd9bf47c6dfc9e1df972255beadbce
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-09 16:35:27 +0100

    database.py : class DataBaseHandler and export methods

--- chess/controllers/database.py
+++ chess/controllers/database.py
@@ -1,62 +1,34 @@
 from chess.models.actors import Actor
 from chess.models.game import Tournament
+from chess.utils.conversion import str_into_date
 from tinydb import TinyDB
+from chess.views.flow import validation_export
 
 
-db_actors = TinyDB('db_actors.json')
-actors_table = db_actors.table("actors")
-actors_table.truncate()	# clear the table first
+class DataBaseHandler:
+    def __init__(self, db):
+        self.db = db
+        self.players_table = db.table
+        self.tournament_table = db.tournament
+        self.tournament_step = None
+
+    def export_actor(self, actor):
+        dictio = actor.actor_to_dict()
+        self.players_table.insert(dictio)
+        chess.views.flow.validation_export(actor)
+
+    def export_tournament(self, tournament, step):
+        dictio = tournament.tournament_to_dict(step)
+        self.tournament_table.insert(dictio)
+        self.tournament_step = step
+        chess.views.flow.validation_export(tournament)
+
+    def import_actor(self, json):
+        pass
+
+    def import_tournament(self):
+        pass
 
 
-def serialize_actors(actors):
-    serialized_actors = []
-    for actor in actors:
-        serialized_actor = {
-            'actor_id': actor.actor_id,
-            'last_name': actor.last_name,
-            'first_name': actor.first_name,
-            'birthdate': actor.birthdate,
-            'gender': actor.gender,
-            'rank': actor.rank,
-            'tournaments': actor.tournaments
-        }
-        serialized_actors.append(serialized_actor)
-    actors_table.insert_multiple(serialized_actors)
-
-
-def import_actors():
-    serialized_actors = actors_table.all()
-    for actor in serialized_actors:
-        actor_id = actor['actor_id']
-        last_name = actor['last_name']
-        first_name = actor['first_name']
-        birthdate = actor['birthdate']
-        gender = actor['gender']
-        rank = actor['rank']
-        tournaments = actor['tournaments']
-        actor = Actor(last_name, first_name, birthdate, gender, rank)
-        actor.actor_id = actor_id
-        actor.tournaments = tournaments
-    return len(serialized_actors)
-
-
-db_tournament = TinyDB('db_tournament')
-tournament_table = db_tournament.table("tournament")
-tournament_table.truncate()	# clear the table first
-
-
-def serialize_tournament(tournament):
-    serialized_tournament = {
-        'tournament_id': tournament.tournament_id,
-        'name': tournament.name,
-        'location': tournament.location,
-        'start_date': tournament.start_date,
-        'end_date': tournament.end_date,
-        'timer_type': tournament.timer_type,
-        'description': tournament.description,
-        'number_of_rounds': tournament.number_of_rounds,
-        'rounds': tournament.rounds,
-        'list_of_players': tournament.list_of_players,
-        'players_assigned': tournament.players_assigned
-    }
-    tournament_table.insert(serialized_tournament)
+if __name__ == '__main__':
+    pass
