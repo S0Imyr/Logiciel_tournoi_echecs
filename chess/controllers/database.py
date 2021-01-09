bb2d599e72532d69382ef801158da7810db4c779
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-09 19:54:47 +0100

    database.py : suppression of class DataBaseHandler function to export/import an actor

--- chess/controllers/database.py
+++ chess/controllers/database.py
@@ -1,34 +1,54 @@
 from chess.models.actors import Actor
 from chess.models.game import Tournament
-from chess.utils.conversion import str_into_date
+from chess.utils.conversion import str_to_date
 from tinydb import TinyDB
-from chess.views.flow import validation_export
 
 
-class DataBaseHandler:
-    def __init__(self, db):
-        self.db = db
-        self.players_table = db.table
-        self.tournament_table = db.tournament
-        self.tournament_step = None
+def export_actor(actor):
+    db = TinyDB('db.json')
+    actors_table = db.table('actors')
+    dictio = actor.actor_to_dict()
+    actors_table.insert(dictio)
 
-    def export_actor(self, actor):
-        dictio = actor.actor_to_dict()
-        self.players_table.insert(dictio)
-        chess.views.flow.validation_export(actor)
 
-    def export_tournament(self, tournament, step):
-        dictio = tournament.tournament_to_dict(step)
-        self.tournament_table.insert(dictio)
-        self.tournament_step = step
-        chess.views.flow.validation_export(tournament)
+def export_tournament(tournament, step):
+    pass
 
-    def import_actor(self, json):
-        pass
 
-    def import_tournament(self):
-        pass
+def import_actors():
+    db = TinyDB('db.json')
+    actors_table = db.table('actors')
+    serialized_actors = actors_table.all()
+    actors = []
+    for value in serialized_actors:
+        actor = Actor(value['last_name'],
+                      value['first_name'],
+                      str_to_date(value['birthdate']),
+                      value['gender'],
+                      value['rank'])
+        actor.dict_to_actor(value)
+        actors.append(actor)
+    return len(serialized_actors), actors
+
+
+def import_tournament():
+    pass
+
+
 
 
 if __name__ == '__main__':
+    import datetime
+    from chess.models.actors import Actor, Player
+
+    TinyDB('db.json').table('actors').truncate()
+    acteur1 = Actor("Skywalker", "Anakin", datetime.date(41, 5, 6), "M", 8)
+
+    export_actor(acteur1)
+    # print(vars(acteur1))
+    acteurs = import_actors()
+    print(acteurs[1])
+    # joueur1 = Player(acteur1, "00000001", 1)
+    # print(vars(joueur1))
+
     pass
