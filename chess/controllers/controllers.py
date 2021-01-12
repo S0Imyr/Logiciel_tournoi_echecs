da48fc4d0a79a60f499793de9a5488f1ccefe8d8
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-12 21:03:59 +0100

    controllers.py : class attribute actors to store {id : actors} This attribute is used to replace specials methods, getitem and contains, and to check if an id_actor already exists.
    Definition of TournamentPause and TournamentInterruption to give the option of
    interrupt and store the tournament progress.

--- chess/controllers/controllers.py
+++ chess/controllers/controllers.py
@@ -2,15 +2,21 @@ import datetime
 
 from chess.models.game import Tournament
 from chess.models.actors import Actor
+from chess.models.database import DataBaseHandler
 
 from chess.views.menuview import MenuView
-from chess.views.flow import view_validation_new_player, view_new_actor, \
+from chess.views.flow import view_validation_new_actor, view_input_new_actor, \
     view_intro_home_menu, view_tournament_creation, view_tournament_players,\
-    view_id_player, view_launch_tournament, view_round_matchs
+    view_id_player, view_launch_tournament, view_round_matchs, \
+    view_validation_actors_imported, view_tournament_final, \
+    view_validation_export_actors, view_validation_players
+
 
 from chess.controllers.menus import Menu
 from chess.controllers.input import tournament_inputs, input_actor, \
     input_match_results, input_tournament_players
+
+from tinydb import TinyDB
 
 
 NB_PLAYERS = 8
@@ -37,34 +43,29 @@ class Actors:
     """
     Gestion des acteurs
     """
+    actors = {}
+
     def __init__(self):
         self.menu = Menu()
         self.view = MenuView(self.menu)
-        self.actors = {}
 
     def __call__(self):
-        view_new_actor()
+        view_input_new_actor()
         actor_arguments = input_actor()
         actor = Actor(actor_arguments[0],
                       actor_arguments[1],
                       actor_arguments[2],
                       actor_arguments[3],
                       actor_arguments[4])
-        self.actors[actor.actor_id] = actor
-        view_validation_new_player(actor)
-        # Enregistrer dans la DB
+        Actors.actors[actor.actor_id] = actor
+        view_validation_new_actor(actor)
 
         self.menu.add("auto", "Ajouter un nouveau joueur", Actors())
+        self.menu.add("auto", "Exporter les joueurs", ExportActors(Actors.actors))
         self.menu.add("auto", "Retour au menu principal", HomeMenuController())
         self.menu.add("q", "Quitter", Ending())
         user_choice = self.view.get_user_choice()
         return user_choice.handler
-
-    def __getitem__(self, item):
-        return self.actors[item]
-
-    def __contains__(self, item):
-        return item in self.actors
 
 
 class HomeMenuController:
@@ -104,7 +105,6 @@ class TournamentCreation:
                                      tournament_arguments[2],
                                      tournament_arguments[3])
         self.tournament.start_date = datetime.date.today()
-        # Export information ? step 0
         return TournamentPlayersMenu(self.tournament)
 
 
@@ -120,13 +120,48 @@ class TournamentPlayersMenu:
     def __call__(self):
         view_tournament_players(self.tournament)
 
-        self.menu.add("auto", "Ajouter les joueurs par id", TournamentPlayers(self.tournament))
-        self.menu.add("auto", "Retour au Menu Principal", HomeMenuController())
+        self.menu.add("auto",
+                      "Ajouter les joueurs par id",
+                      TournamentPlayers(self.tournament))
+        self.menu.add("auto",
+                      "Interrompre le tournoi",
+                      TournamentPause(self.tournament))
+        self.menu.add("auto",
+                      "Retour au Menu Principal",
+                      HomeMenuController())
         self.menu.add("q", "Quitter", Ending())
 
         user_choice = self.view.get_user_choice()
-
         return user_choice.handler
+
+
+class TournamentPause:
+    def __init__(self, tournament):
+        self.tournament = tournament
+        self.menu = Menu()
+        self.view = MenuView(self.menu)
+
+    def __call__(self):
+        self.menu.add("auto",
+                      "Continuer et passer au tour suivant",
+                      LaunchTournament(self.tournament))
+        self.menu.add("auto",
+                      "Interrompre le tournoi",
+                      TournamentInterruption(self.tournament))
+
+        user_choice = self.view.get_user_choice()
+        return user_choice.handler
+
+
+class TournamentInterruption:
+    def __init__(self, tournament):
+        self.tournament = tournament
+        handler = DataBaseHandler(TinyDB('db.json'))
+        handler.database.table('tournament').truncate()
+        handler.export_tournament(self.tournament)
+
+    def __call__(self):
+        return Ending()
 
 
 class TournamentPlayers:
@@ -137,20 +172,22 @@ class TournamentPlayers:
         self.tournament = tournament
 
     def __call__(self):
-        players = []
-        while len(self.tournament.list_of_players) < NB_PLAYERS:
-            num_player = len(self.tournament.list_of_players) + 1
+        actors = []
+        actors_id = []
+        while len(actors) < NB_PLAYERS:
+            num_player = len(actors) + 1
             view_id_player(self.tournament, num_player)
-            message = f"Veuillez indiquer "
-            f"l'identifiant du joueur {num_player}: "
-            actor_id = input_tournament_players(num_player, message)
-            while actor_id not in Actors():  ######
+            message = f"Veuillez indiquer " \
+                      f"l'identifiant du joueur {num_player}: "
+            actor_id = input_tournament_players(message)
+            while actor_id not in Actors.actors:
                 message += f"Identifiant inconnu."
-                actor_id = input_tournament_players(num_player, message)
-            players.append(actor_id)
-            # view pour valider les joueur mis
-        self.tournament.define_players(players)
-        # Export
+                actor_id = input_tournament_players(message)
+            actors_id.append(actor_id)
+            actor = Actors.actors[actor_id]
+            actors.append(actor)
+        self.tournament.define_players(actors)
+        view_validation_players(actors)
         return LaunchTournament(self.tournament)
 
 
@@ -159,19 +196,25 @@ class LaunchTournament:
         self.tournament = tournament
 
     def __call__(self):
-        view_launch_tournament(self.tournament)                                   # Affichage lancement tournoi
-        for num_round in range(NB_ROUND):
-            self.tournament.start_round(num_round)                                # Instance de round et définition des matchs
+        num_round = len(self.tournament.rounds)
+        if num_round == 0:
+            view_launch_tournament(self.tournament)                               # Affichage lancement tournoi
+        if num_round == 4:
+            view_tournament_final()
+        else:
+            self.tournament.init_round(num_round)                                 # Instance de round et définition des matchs
             view_round_matchs(self.tournament.rounds[num_round])                  # Affichage des matchs
             winners = input_match_results(self.tournament.rounds[num_round])      # Attente, entrée des gagnants
             self.tournament.register_round_results(num_round, winners)            # Entrées dans instance de round
             view_round_matchs(self.tournament.rounds[num_round])                  # Affichage résultats
-            # step += 1
-            # Export()                                                            # Export TDB
+            return TournamentPause(self.tournament)                               # Export TDB ?
 
 
 class ResumeTournament:
-    pass
+    def __call__(self):
+        handler = DataBaseHandler(TinyDB('db.json'))
+        tournament = handler.import_tournament()
+        return LaunchTournament(tournament)
 
 
 class ImportActors:
@@ -179,11 +222,23 @@ class ImportActors:
         pass
 
     def __call__(self):
-        pass
-        """serialized_actors = actors_table.all()
-        nb_actors = len(serialized_actors)
-        chess.views.flow.view_import_actors(nb_actors)
-        return HomeMenuController()"""
+        handler = DataBaseHandler(TinyDB('db.json'))
+        num_actors, actors = handler.import_actors()
+        view_validation_actors_imported(actors)
+        for actor in actors:
+            Actors.actors[actor.actor_id] = actor
+        return HomeMenuController()
+
+
+class ExportActors:
+    def __init__(self, actors):
+        self.actors = actors
+
+    def __call__(self):
+        handler = DataBaseHandler(TinyDB('db.json'))
+        for actor in self.actors:
+            handler.export_actor(actor)
+        view_validation_export_actors(self.actors)
 
 
 class RapportMenu:
