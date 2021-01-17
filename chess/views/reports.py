d5e5943147578a8e8e219583b6d3e5ccc4fa455b
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-17 16:21:33 +0100

    reports.py : report_actors_by_alpha display the players

--- chess/views/reports.py
+++ chess/views/reports.py
@@ -1,7 +1,12 @@
+from operator import attrgetter
 
 
-def report_actors_by_alpha():
-    pass
+def report_actors_by_alpha(actorslist):
+    sorted_actors = sorted(actorslist, key=attrgetter("first_name"))
+    sorted_actors = sorted(sorted_actors, key=attrgetter("last_name"))
+    for actor in sorted_actors:
+        print(f"{actor.last_name} {actor.first_name}: "
+              f"Nombre de tournoi joués : {len(actor.tournaments)}")
 
 
 def report_actors_by_rank():
