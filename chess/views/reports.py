fff11fb3d954a2fdc5cd8c0331b3fb5826ff5b09
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-17 16:51:23 +0100

    reports.py : definition of players_table to print a table with a list of actors and in a same way : tournament_table. The two methods report_actors use players_table

--- chess/views/reports.py
+++ chess/views/reports.py
@@ -1,16 +1,35 @@
 from operator import attrgetter
 
 
-def report_actors_by_alpha(actorslist):
-    sorted_actors = sorted(actorslist, key=attrgetter("first_name"))
+def players_table(actors_list):
+    for actor in actors_list:
+        name_characters = len(actor.last_name)+len(actor.first_name)
+        rank_characters = len(str(actor.rank))
+        print(f"{actor.last_name} {actor.first_name}"
+              + " " * (30 - name_characters) +
+              f"Classement : rank {actor.rank}"
+              + " " * (10 - rank_characters) +
+              f"Tournoi joués : {len(actor.tournaments)}")
+
+
+def tournaments_table(tournaments_list):
+    for tournament in tournaments_list:
+        id_characters = len(tournament.tournament_id)
+        #dates_characters = len(tournament.start_date)+len(tournament.end_date)
+        print(f"Tournoi: {tournament.tournament_id}"
+              + " " * (10 - id_characters) +
+              f"Du: {tournament.start_date} au: {tournament.end_date}")
+
+
+def report_actors_by_alpha(actors_list):
+    sorted_actors = sorted(actors_list, key=attrgetter("first_name"))
     sorted_actors = sorted(sorted_actors, key=attrgetter("last_name"))
-    for actor in sorted_actors:
-        print(f"{actor.last_name} {actor.first_name}: "
-              f"Nombre de tournoi joués : {len(actor.tournaments)}")
+    players_table(sorted_actors)
 
 
-def report_actors_by_rank():
-    pass
+def report_actors_by_rank(actors_list):
+    sorted_actors = sorted(actors_list, key=attrgetter("rank"))
+    players_table(sorted_actors)
 
 
 def report_tournaments_list():
