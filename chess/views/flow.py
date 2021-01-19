c8d9cda84216aeb6fabd637288e7bbc1cdd459fd
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-19 12:01:57 +0100

    flow.py : rename variable and input_match_results

--- chess/views/flow.py
+++ chess/views/flow.py
@@ -29,9 +29,9 @@ def view_tournament_players(tournament):
 
 
 def view_input_new_actor():
-    display = "\n ### Nouveau joueur ### \n"
-    display += "\n -- Entrez les informations demandées: --\n"
-    print(display)
+    view = "\n ### Nouveau joueur ### \n"
+    view += "\n -- Entrez les informations demandées: --\n"
+    print(view)
 
 
 def view_validation_new_actor(actor):
@@ -46,10 +46,10 @@ def view_id_player(tournament, num=1):
 
 def view_validation_players(players):
     print(f"-- Les joueurs du tournoi sont : --")
-    message = ""
+    view = ""
     for player in players:
-        message += "\n" + str(player)
-    print(message)
+        view += "\n" + str(player)
+    print(view)
 
 
 def view_launch_tournament(tournament):
@@ -57,34 +57,34 @@ def view_launch_tournament(tournament):
 
 
 def view_round_matchs(r0und):
-    display = f"\n ### Matchs du Tour {r0und.round_nb + 1} " \
+    view = f"\n ### Matchs du Tour {r0und.round_nb + 1} " \
               f"du Tournoi {r0und.tournament_id} : ### \n \n"
     if r0und.matchs != {}:
         if r0und.finished:
-            display += f"Les matchs ont vu s'affronter : \n"
+            view += f"Les matchs ont vu s'affronter : \n"
         else:
-            display += f"Les matchs verront s'affronter : \n"
+            view += f"Les matchs verront s'affronter : \n"
         for num_match in range(NB_MATCH):
-            display += f"{num_match + 1}. " \
+            view += f"{num_match + 1}. " \
                        f"{r0und.matchs[num_match].player1.name}" \
                        f" et {r0und.matchs[num_match].player2.name} \n"
             if r0und.finished:
                 if r0und.matchs[num_match].winner == 0:
-                    display += f"Match nul. \n"
+                    view += f"Match nul. \n"
                 if r0und.matchs[num_match].winner == 1:
-                    display += f"Victoire de " \
+                    view += f"Victoire de " \
                                f"{r0und.matchs[num_match].player1.name}. \n"
                 if r0und.matchs[num_match].winner == 2:
-                    display += f"Victoire de " \
+                    view += f"Victoire de " \
                                f"{r0und.matchs[num_match].player2.name}. \n"
-    print(display)
+    print(view)
 
 
 def view_round_results(r0und):
     pass
 
 
-def input_match_results():
+def view_wait_match_results():
     print("### En attente de résultats: ### \n"
           "\n"
           "Lorsqu'un match est terminé, "
@@ -97,36 +97,36 @@ def view_tournament_final(tournament):
               "\n"
               "-- Classement final -- \n")
     for rank in range(1, len(tournament.list_of_players) + 1):
-        message = ""
+        view = ""
         for player in tournament.list_of_players:
             if rank == 1:
                 if player.place == rank:
-                    message += f"1er : {player.name}"\
+                    view += f"1er : {player.name}"\
                                + " " * (20-len(player.name))\
                                + "{player.points}"
             else:
                 if player.place == rank:
-                    message += f"{rank}eme: {player.name}"
-                    message += " " * (20 - len(player.name))
-        print(message)
+                    view += f"{rank}eme: {player.name}"
+                    view += " " * (20 - len(player.name))
+        print(view)
 
 
 def view_validation_actors_imported(actors):
     print(f"\n ### Import de joueurs ### \n")
     if len(actors) == 0:
-        message = "Base de donnée vide, aucun import possible"
+        view = "Base de donnée vide, aucun import possible"
     else:
-        message = f"-- {len(actors)} joueurs importés --"
-    print(message)
+        view = f"-- {len(actors)} joueurs importés --"
+    print(view)
     for actor in actors:
         print(actor)
 
 
 def view_validation_actors_exported(exported_actors):
-    message = f"Les {len(exported_actors)} personnes ont été exportés \n"
+    view = f"Les {len(exported_actors)} personnes ont été exportés \n"
     for actor in exported_actors:
-        message += "\n" + actor.actor_id + " " + actor.first_name + " " + actor.last_name
-    print(message)
+        view += "\n" + actor.actor_id + " " + actor.first_name + " " + actor.last_name
+    print(view)
 
 
 def view_import_no_tournament():
