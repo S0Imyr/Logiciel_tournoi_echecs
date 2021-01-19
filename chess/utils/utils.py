652f21c1a520477a6ef852f11078d247a0ebd3b2
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-19 21:05:02 +0100

    utils.py : function to determine the last id in a list of id.

--- chess/utils/utils.py
+++ chess/utils/utils.py
@@ -11,7 +11,10 @@ def get_new_id(identifier, width):
 def get_last_id(list_of_id, width):
     last_number = 0
     for identifier in list_of_id:
-        last_number = max(last_number, int(identifier.lstrip('0')))
+        if identifier == "":
+            last_number = 0
+        else:
+            last_number = max(last_number, int(identifier.lstrip('0')))
     last = (width - len(str(last_number))) * "0" + str(last_number)
     return last
 
@@ -21,3 +24,4 @@ if __name__ == '__main__':
     print(get_new_id("00000000", 8))
 
     print(get_last_id(["000000001", "000000002", "000000003", "000000004", "000000006"], 8))
+    print(get_last_id([], 8))
