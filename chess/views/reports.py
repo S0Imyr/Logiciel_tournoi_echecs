0fcd3df181321ed7634231515478e85a9c50de3b
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-20 22:39:25 +0100

    reports.py : functions : report_tournament_players report_tournament_matchs report_tournament_rounds

--- chess/views/reports.py
+++ chess/views/reports.py
@@ -12,10 +12,6 @@ def players_table(actors_list):
               f"Tournoi joués : {len(actor.list_of_tournaments_played)}")
 
 
-def tournaments_table(tournaments_list):
-    pass
-
-
 def report_actors_by_alpha(actors_list):
     sorted_actors = sorted(actors_list, key=attrgetter("first_name"))
     sorted_actors = sorted(sorted_actors, key=attrgetter("last_name"))
@@ -39,16 +35,28 @@ def report_tournaments_list(tournaments_list):
               f"Du: {tournament.start_date} au: {tournament.end_date}")
 
 
-def report_tournament_players():
-    pass
+def report_tournament_players(tournament, sort):
+    actors_list = []
+    for player in tournament.list_of_players:
+        actors_list.append(player.actor)
+    if sort == "Alphabetical":
+        report_actors_by_alpha(actors_list)
+    elif sort == "By rank":
+        report_actors_by_rank(actors_list)
 
 
-def report_tournament_matchs():
-    pass
+def report_tournament_matchs(tournament):
+    print(f"### Tournoi: {tournament.name} ###")
+    for r0und in tournament.rounds:
+        print(f"Les matchs du tour {r0und.round_nb}: \n")
+        for match in r0und.matchs.values():
+            print(str(match))
 
 
-def report_tournament_rounds():
-    pass
+def report_tournament_rounds(tournament):
+    print(f"### Tournoi: {tournament.name} ###")
+    for r0und in tournament.rounds:
+        print(str(r0und))
 
 
 def report_no_tournament():
