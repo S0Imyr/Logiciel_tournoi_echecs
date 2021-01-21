e4fdc36b11224cc77a807941d53bb2e830e70644
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-21 12:09:16 +0100

    PEP8

--- chess/utils/conversion.py
+++ chess/utils/conversion.py
@@ -8,11 +8,13 @@ def str_into_date(str_date):
     year = int(date[2])
     return datetime.date(year, month, day)
 
+
 def list_to_str_space(list):
     string = ""
     for element in list:
-        string += str(element)+ " "
+        string += str(element) + " "
     return string.strip()
+
 
 def str_space_to_list(string):
     string_list = string.split(" ")
@@ -23,6 +25,7 @@ def str_space_to_list(string):
                 list.append(element)
     return list
 
+
 def str_space_to_int_list(string):
     string_list = string.split(" ")
     int_list = []
@@ -31,6 +34,7 @@ def str_space_to_int_list(string):
             if element != '':
                 int_list.append(int(element))
     return int_list
+
 
 def str_to_date(string):
     if string == 'None':
@@ -41,11 +45,3 @@ def str_to_date(string):
         month = int(date_list[1])
         day = int(date_list[2])
         return datetime.date(year, month, day)
-
-if __name__ == "__main__":
-    test = list_to_str_space([2, 1])
-    print(test)
-    print(str_space_to_list(test))
-    print(str_space_to_list(' '))
-    da = str_to_date("0019-12-07")
-    print(da.month)
