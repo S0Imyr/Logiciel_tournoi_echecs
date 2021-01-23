60f38f1b132abcac01e339db3d01c7bc1fd83fd9
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-01-23 22:45:25 +0100

    docstrings

--- chess/utils/conversion.py
+++ chess/utils/conversion.py
@@ -1,7 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+This module handles the different conversion needed.
+"""
+
+
 import datetime
 
 
 def str_into_date(str_date):
+    """ Converts a string into a date.
+
+    :param str_date: string date with a format : **/**/****
+    :return: the instance date for the module datetime.
+
+    """
     date = str_date.split("/")
     day = int(date[0])
     month = int(date[1])
@@ -10,6 +24,13 @@ def str_into_date(str_date):
 
 
 def list_to_str_space(list):
+    """ Converts a list in a spaced string.
+
+    The string consists of the succession of the elements of the list separated by spaces.
+
+    :param list: the list to convert.
+    :return: the spaced string.
+    """
     string = ""
     for element in list:
         string += str(element) + " "
@@ -17,6 +38,13 @@ def list_to_str_space(list):
 
 
 def str_space_to_list(string):
+    """ Converts a spaced string in a list.
+
+    The string consists of the succession of the elements of the list separated by spaces.
+
+    :param string: the string to convert.
+    :return: the corresponding list.
+    """
     string_list = string.split(" ")
     list = []
     if string_list != '':
@@ -27,6 +55,13 @@ def str_space_to_list(string):
 
 
 def str_space_to_int_list(string):
+    """ Converts a spaced string in a list of integer.
+
+    The string consists of the succession of the elements of the list separated by spaces.
+
+    :param string: the string to convert.
+    :return: the corresponding list of integer.
+    """
     string_list = string.split(" ")
     int_list = []
     if string_list != '':
@@ -37,6 +72,12 @@ def str_space_to_int_list(string):
 
 
 def str_to_date(string):
+    """ Converts a string into a date.
+
+    :param string: string date with a format : **-**-****
+    :return: the instance date for the module datetime.
+
+    """
     if string == 'None':
         return None
     else:
