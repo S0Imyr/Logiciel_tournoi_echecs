d2ea3130b53a450dbbc40aa138e66eee6d95f495
rlossec <raphaelossec@gmail.com>
2024-03-21 15:05:06 +0100

    remove .idea/ and chess/ files

--- src/chess/models/constant.py
+++
@@ -1,8 +0,0 @@
-FIRST = 1
-ID_WIDTH = 8
-NB_PLAYERS = 8
-NB_MATCH = 4
-NB_ROUND = 4
-POINTS = {"victory": 1, "draw": 0.5, "defeat": 0}
-MATCH_1ST_ROUND = [[1, 5], [2, 6], [3, 7], [4, 8]]
-MATCH_OTHER_ROUND = [[1, 2], [3, 4], [5, 6], [7, 8]]
